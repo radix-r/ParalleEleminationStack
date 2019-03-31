af350259effe620a387925e5b36be467c9a5369b
Ross <rosscwgner@gmail.com>
2019-03-31 17:03:06 -0400

    EliminationArray done

---
+++ src/EliminationArray.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class EliminationArray<T> {
+
+    private static final int DURATION = 100; // how avoid magic number?
+    LockFreeExchanger<T>[] exchanger;
+    Random random;
+
+    public EliminationArray(int capacity){
+        exchanger = (LockFreeExchanger<T>[]) new LockFreeExchanger[capacity];
+        // init the exchanger
+        for(int i = 0; i< capacity; i++){
+            exchanger[i] = new LockFreeExchanger<T>();
+        }
+        random = new Random();
+    }
+
+    public T visit(T value, int range) throws TimeoutException{
+        int slot = random.nextInt(range);
+        return (exchanger[slot].exchange(value,DURATION, TimeUnit.MILLISECONDS));
+    }
+
+}
